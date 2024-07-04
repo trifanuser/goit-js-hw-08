0aa57ab7318884d52297d1e29fb88c8508db627f
trifanuser <flc.tri@gmail.com>
2024-07-04 12:01:21 +0300

    modificari gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const link = document.createElement('a');
+  link.href = item.original;
+  link.classList.add('gallery__link');
+
+  const image = document.createElement('img');
+  image.src = item.preview;
+  image.alt = item.description;
+  image.classList.add('gallery__image');
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+  gallery.appendChild(galleryItem);
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
