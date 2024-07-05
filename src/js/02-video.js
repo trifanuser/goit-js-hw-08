f407459758fd7172abe3acc6c586e23112d127da
trifanuser <flc.tri@gmail.com>
2024-07-05 18:58:13 +0300

    modificari video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,21 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
 
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+function saveCurrentTime(data) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+}
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(error => {
+    console.error('Failed to set current time:', error);
+  });
+}
